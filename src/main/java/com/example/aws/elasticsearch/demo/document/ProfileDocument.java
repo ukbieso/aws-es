adfc1ffd1979891c9259dcc4186c1973b7634b43
Ukbay Iyasu <ukbay.gebremeskel@ruter.no>
2019-03-18 21:49:00 +0100

    initial commit

---
+++ src/main/java/com/example/aws/elasticsearch/demo/document/ProfileDocument.java
@@ -0,0 +1,15 @@
+package com.example.aws.elasticsearch.demo.document;
+
+import com.example.aws.elasticsearch.demo.model.Technologies;
+        import lombok.Data;
+
+        import java.util.List;
+
+@Data
+public class ProfileDocument {
+
+    private String id;
+    private String name;
+
+
+}
